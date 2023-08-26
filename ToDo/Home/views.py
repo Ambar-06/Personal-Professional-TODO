d242ae0f8fd9edb1b0928b327090ec3799c42a02
Ambar <brannstrom9911@gmail.com>
2023-08-27 01:15:11 +0530

    Initial Commit

---
+++ ToDo/Home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import permission_classes, authentication_classes, api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+
+# Create your views here.
+@api_view(['GET'])
+def test_f(request):
+    json_data = {
+        "status" : 200,
+        "data" : '',
+        "message" : 'Working well.',
+    }
+    return Response(json_data, status=status.HTTP_200_OK)
