fb8afe440bfbad0143af67bdbfe4127cb22c5478
Ambar <brannstrom9911@gmail.com>
2023-08-27 23:51:26 +0530

    Imporved Backend

--- ToDo/Home/views.py
+++ ToDo/Home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.decorators import permission_classes, authentication_classes, api_view
 from rest_framework.response import Response
@@ -7,9 +7,11 @@ from rest_framework import status
 from pydantic import ValidationError
 from django.contrib import messages
 
+import hashlib
 
-from schemas import *
-from models import *
+
+from .schemas import *
+from .models import *
 
 
 # Create your views here.
@@ -28,25 +30,42 @@ def signup_f(request):
         return render(request, 'signup.html')
     elif request.method == 'POST':
         try:
-            validated_data = UserSignUpDataSchema(**request.data)
-        except ValidationError as e:
-            messages.error(request, 'Invalid Data, Please share valid data.')
-            return render(request, 'signup.html')
-        SignUpName = validated_data.signupname
-        SignUpEmail = validated_data.signupemail
-        SignUpNumber = validated_data.signupnumber
-        SignUpPassword = validated_data.signuppassword
-        SignUpConfirmPassword = validated_data.confirmpassword
+            try:
+                validated_data = UserSignUpDataSchema(**request.data)
+            except ValidationError as e:
+                messages.error(request, 'Invalid Data, Please share valid data.')
+                return render(request, 'signup.html')
+            SignUpName = validated_data.signupname
+            SignUpEmail = validated_data.signupemail
+            SignUpNumber = validated_data.signupnumber
+            SignUpPassword = validated_data.signuppassword
+            SignUpConfirmPassword = validated_data.confirmpassword
 
-        if SignUpPassword != SignUpConfirmPassword:
-            messages.error(request, 'Password did not match.')
-            return render(request, 'signup.html')
-        
+            if SignUpPassword != SignUpConfirmPassword:
+                messages.error(request, 'Password did not match.')
+                return render(request, 'signup.html')
+            
+            password_encoded = SignUpPassword.encode() 
 
-        
+            password_hashed = hashlib.md5(password_encoded).hexdigest()
+            
+            UserData = UserSignUpModel(
+                    SignUpName=SignUpName,
+                    SignUpEmail=SignUpEmail,
+                    SignUpNumber=SignUpNumber,
+                    SignUpPassword=password_hashed
+                )
+            UserData.save()
+            messages.success(request, 'Your email has been registered successfully. Login to continue')
+            return render(request, 'signup.html')
+        except Exception as e:
+            print(e)
+            return HttpResponse(f'<h1>{e}</h1>')
 
 
 @api_view(['GET', 'POST'])
 def login_f(request):
     if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
         return render(request, 'login.html')
